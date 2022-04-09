d17464d16c2f0b72336df1301dfd16c163adb645
claukers <claukers@github>
2022-04-09 00:39:58 -0400

    remove renderTemplate export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export {setCache, getTemplateLocation} from "./cache.js";
 export {Component, ComponentState} from "./component.js";
 export {PathRouter, PathRouterState, RouteLink, historyPushPath, RouteLinkState} from "./router.js";
 export {Store, Action, Reducer, StoreReducerMap, StoreListener, Selector, StoreOptions} from "./store.js";
-export {renderTemplate} from "./template.js";
