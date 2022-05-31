08cb77fcc0383cd221a8f07ac21a8aefc2fc4f9e
claukers <claukers@github>
2022-05-31 03:30:16 -0400

    added missing interface exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-export {defineFunction, FunctionComponent} from "./function/index.js";
+export {
+  defineFunction,
+  FunctionComponent,
+  FunctionMeta,
+  RenderContext,
+  SetFunction,
+  UseEffectFunction,
+  UseStateFunction,
+  FunctionComponentOutput,
+  RenderFunctionArgs
+} from "./function/index.js";
 export {Component} from "./component/index.js";
 export {ComponentState, IComponent, setCache, set, get, render, disconnect} from "./template/index.js";
 export {PathRouter, PathRouterState, RouteLink, historyPushPath, RouteLinkState} from "./router/index.js";
