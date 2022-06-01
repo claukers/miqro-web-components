aa6669e7a3027c034e7ebaddd3162ef00c194053
claukers <claukers@github>
2022-06-01 01:15:03 -0400

    added setLogLevel

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,4 @@ export {Component} from "./component/index.js";
 export {ComponentState, IComponent, setCache, set, get, render, disconnect} from "./template/index.js";
 export {PathRouter, PathRouterState, RouteLink, historyPushPath, RouteLinkState} from "./router/index.js";
 export {Action, Reducer, StoreReducerMap, StoreListener, Selector, StoreOptions, Store} from "./store.js";
+export {setLogLevel} from "./log.js";
--- src/log.ts
+++ src/log.ts
@@ -19,13 +19,22 @@ const LOG_LEVEL_PRIORITY = {
   none: 5
 };
 
-let currentLogLevel = LOG_LEVEL_PRIORITY.warn;
+const DEFAULT_LEVEL = LOG_LEVEL_PRIORITY.warn;
+
+let currentLogLevel: number | null = null;
 
 export function setLogLevel(level = "info") {
-
+  if (currentLogLevel === null) {
+    currentLogLevel = DEFAULT_LEVEL;
+  } else {
+    throw new Error("cannot change LOG_LEVEL at this stage!");
+  }
 }
 
 export function log(level: LOG_LEVEL_STRING, text: string | any, ...args: any[]) {
+  if (currentLogLevel === null) {
+    currentLogLevel = DEFAULT_LEVEL;
+  }
   if (currentLogLevel <= LOG_LEVEL_PRIORITY[level]) {
     console[level](text, ...args);
   }
