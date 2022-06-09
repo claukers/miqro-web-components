16b35eed9b126f41ea86954b142483e3f66a0e31
claukers <claukers@github>
2022-06-09 12:00:59 -0400

    useSubscription now aborts when async function component state changes

--- src/function/use/subscribe.ts
+++ src/function/use/subscribe.ts
@@ -1,18 +1,25 @@
-import { Selector, Store } from "../../store.js";
-import { RenderFunctionArgs } from "../../template/utils/template.js";
-import { ContextCall, FunctionComponentMeta, FunctionComponentThis } from "../common.js";
+import {Selector, Store} from "../../store.js";
+import {RenderFunctionArgs} from "../../template/utils/template.js";
+import {ContextCall, FunctionComponentMeta, FunctionComponentThis} from "../common.js";
 
 export function useSubscription<R, S>(this: FunctionComponentThis, element: HTMLElement, context: ContextCall, meta: FunctionComponentMeta, renderArgs: RenderFunctionArgs, store: Store<S>, selector: Selector<S, R>): R {
-    const [value, setValue] = this.useState(selector(store.getState()));
-    this.useEffect(() => {
-        function listener(newValue: R) {
-            setValue(newValue);
-        }
+  const [value, setValue] = this.useState(selector(store.getState()));
+  this.useEffect(() => {
+    function listener(newValue: R) {
+      setValue(newValue);
+    }
 
-        store.subscribe(selector, listener);
-        return () => {
-            store.unSubscribe(listener);
-        }
-    });
-    return value;
+    store.subscribe(selector, listener);
+    return () => {
+      store.unSubscribe(listener);
+    }
+  });
+  context.lastValue = value;
+  context.checkChanged = function () {
+    return {
+      shouldAbort: context.lastValue !== selector(store.getState()),
+      shouldRefresh: false
+    }
+  }
+  return value;
 }
