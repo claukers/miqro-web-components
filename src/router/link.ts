f86d84b5d63619e3776480e23ab76f8d1bef552a
claukers <claukers@github>
2022-06-08 23:01:27 -0400

    remove old router impl

--- src/router/link.ts
+++ src/router/link.ts
@@ -1,62 +1,9 @@
-import { Component } from "../component/index.js";
 import { historyPushPath } from "./history.js";
 import { isPathLocation } from "./utils.js";
 import { windowAddEventListener, windowRemoveEventListener } from "../template/utils";
 import { FunctionComponentThis } from "../function/common.js";
 
-/*export interface RouteLinkState {
-  active?: boolean;
-}
-
-export class RouteLink<S extends RouteLinkState = RouteLinkState> extends Component<S> {
-  private readonly clickListener: (ev: Event) => void;
-
-  public static tagName: string = "route-link";
-  private readonly popStateListener: () => void;
-
-  constructor() {
-    super();
-    this.popStateListener = () => {
-      const isActive = isPathLocation(this.dataset.path);
-      if (this.state.active !== isActive) {
-        this.setState({
-          active: isActive
-        } as Partial<S>);
-      }
-    };
-    this.clickListener = (ev) => {
-      ev.preventDefault();
-      historyPushPath(this.dataset.path as string);
-    };
-  }
-
-  public connectedCallback() {
-    this.addEventListener("click", this.clickListener);
-    windowAddEventListener("popstate", this.popStateListener);
-    const isActive = isPathLocation(this.dataset.path);
-    if (this.state.active !== isActive) {
-      this.state.active = isActive;
-    }
-    return super.connectedCallback();
-  }
-
-  public disconnectedCallback() {
-    this.removeEventListener("click", this.clickListener);
-    windowRemoveEventListener("popstate", this.popStateListener);
-  }
-
-  public render() {
-    if (this.classList.contains("active") && !this.state.active) {
-      this.classList.remove("active");
-    } else if (!this.classList.contains("active") && this.state.active) {
-      this.classList.add("active");
-    }
-    return super.render();
-  }
-}*/
-
 export function RouteLink(this: FunctionComponentThis) {
-  const classes = this.useAttribute("class");
   const path = this.useAttribute("data-path");
   const [isActive, setIsActive, getIsActive] = this.useState(false);
 
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,75 +1,8 @@
-import {Component} from "../component/index.js";
-//import { Route, updateRouter, getActiveRoute } from "./utils.js";
 import {Route, getActiveRoute} from "./utils.js";
 import {windowAddEventListener, windowRemoveEventListener} from "../template/utils";
 import {FunctionComponentThis} from "../function/common.js";
 
 const renderTag = (tagName: string | (() => string)) => typeof tagName === "function" ? tagName() : `<${tagName}/>`;
-
-/*export interface PathRouterState {
-  active?: Route;
-  defaultElement?: string | (() => string);
-  routes: Route[];
-}
-
-export class PathRouter<S extends PathRouterState = PathRouterState> extends Component<S> {
-  public static tagName: string = "path-router";
-
-  private readonly popStateListener: () => void;
-
-  constructor() {
-    super();
-    this.popStateListener = () => {
-      updateRouter(this);
-    };
-    this.state.routes = [];
-  }
-
-  public resetRoutes(): void {
-    return this.setState({
-      routes: [] as Route[],
-      active: undefined
-    } as Partial<S>);
-  }
-
-  public setDefault(defaultElement: string): void {
-    return this.setState({
-      defaultElement
-    } as Partial<S>);
-  }
-
-  public pushRoute(route: { path: string; element: string; } | Array<{ path: string; element: string; }>): void {
-    return this.concatRoutes(route);
-  }
-
-  public concatRoutes(route: { path: string; element: string; } | Array<{ path: string; element: string; }>): void {
-    route = route instanceof Array ? route : [route];
-    const routes = this.state.routes.concat(route);
-    return this.setState({
-      routes
-    } as Partial<S>);
-  }
-
-  public connectedCallback() {
-    updateRouter(this, false);
-    windowAddEventListener("popstate", this.popStateListener);
-    return super.connectedCallback();
-  }
-
-  public stateChangedCallback(prevState: S) {
-    if (updateRouter(this)) {
-      return false;
-    }
-  }
-
-  public disconnectedCallback(): void {
-    windowRemoveEventListener("popstate", this.popStateListener);
-  }
-
-  public render() {
-    return this.state.active ? renderTag(this.state.active.element) : (this.state.defaultElement ? renderTag(this.state.defaultElement) : "");
-  }
-}*/
 
 export function PathRouter(this: FunctionComponentThis) {
   const routesJSON = this.useAttribute("data-routes");
