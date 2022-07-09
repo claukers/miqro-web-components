f116784d68a421f553b2ae876c010da3838976bb
claukers <claukers@github>
2022-07-09 02:40:54 -0400

    examples/sfc using deno as bundler

--- examples/sfc/dist/index-component.js
+++
@@ -1,5 +0,0 @@
-
-
-export default function () {
-      this.useAs("te\"xt", "helloWorld");
-    }
--- examples/sfc/dist/index.js
+++
@@ -1,4 +0,0 @@
-import {define} from "../esm.bundle.js";
-import render from "./index-component.js";
-
-define("my-component", render, {"template":"<p>{text}</p><p>label</p><div><p>\"{text}\\\"</p>\n<p>label</p></div>"});
